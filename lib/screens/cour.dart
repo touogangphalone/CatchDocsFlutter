922223ab83a1c85d0fc2094549fce101eb02aa44
séraphin-git <seraphingambo697@gmail.com>
2023-03-29 15:14:52 +0100

    cour screens

--- lib/screens/cour.dart
+++ lib/screens/cour.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_cards/flutter_custom_cards.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:quizstar/screens/style/theme.dart';
 
 class CourScreens extends StatefulWidget {
@@ -14,42 +15,110 @@ class CourScreens extends StatefulWidget {
 
 class _CourScreensState extends State<CourScreens> {
   @override
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text('Custom '),
+        title: Text("cour"),
       ),
-      body: SingleChildScrollView(
+      body: Container(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
+          padding: const EdgeInsets.all(10.0),
+          child: GridView(
             children: [
-              Text('Cour'),
-              Center(
-                child: Wrap(
-                  //mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomCard(
-                      height: 150,
-                      width: 150,
-                      elevation: 5,
-                    ),
-                    SizedBox(width: 5),
-                    CustomCard(
-                      Icons.book_online_rounded,
-                      height: 150,
-                      width: 150,
-                      elevation: 5,
-                      color: Colors.blue,
-                      borderColor: Colors.white,
-                      borderWidth: 2,
-                    ),
-                  ],
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/icons/signup.svg",
+                        height: 60,
+                      ),
+                      Text(
+                        "Mathématiques",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(height: 25),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.indigo),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/icons/signup.svg",
+                        height: 60,
+                      ),
+                      Text(
+                        "Informatiques",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.settings,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Settings",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.book,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Books",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
           ),
         ),
       ),
